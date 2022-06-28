455f528c46b7e7b4541bf02b9198eea3a1ffee58
UrUrZa <IshvarIndrovich@mail.ru>
2022-06-28 16:14:58 +0400

    finally works wellnes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,6 @@ import java.awt.*;
 import java.util.HashMap;
 
 public class Main {
-        public static HashMap<WayPoints, Color> pointsColorMap = new HashMap();
         public static void main(String[] args) {
             SearchResultPool searchResults = new SearchResultPool();
                try {
